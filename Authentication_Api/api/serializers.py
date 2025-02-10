86bc39d48b500da56fc454a538d83094c133d204
Aditya-fxis <adityapatel@fxis.ai>
2025-02-10 11:45:00 +0530

    user registration part completed

--- Authentication_Api/api/serializers.py
+++ Authentication_Api/api/serializers.py
@@ -9,3 +9,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs={
             'password':{'write_only':True}
         }
+    
+    # Validating password and Confirm Password while Registration
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password doesn't match")
+        return attrs
+    
+    def create(self, validate_data):
+        return User.objects.create_user(**validate_data)
